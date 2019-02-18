6d75f833be4cd91e091471e7ebf80434d7fff55d
Cole Gerdemann <corvettecole@gmail.com>
2019-02-18 13:29:55 -0500

    Add options for european date format and displaying the decimal point in temperature

--- phoneApp/src/main/java/com/corvettecole/pixelwatchface/MainActivity.java
+++ phoneApp/src/main/java/com/corvettecole/pixelwatchface/MainActivity.java
@@ -16,6 +16,8 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.anjlab.android.iab.v3.BillingProcessor;
+import com.anjlab.android.iab.v3.TransactionDetails;
 import com.google.android.gms.tasks.Task;
 import com.google.android.gms.wearable.DataClient;
 import com.google.android.gms.wearable.DataEvent;
@@ -29,7 +31,7 @@ import com.google.android.gms.wearable.Wearable;
 
 import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity /*implements DataClient.OnDataChangedListener*/ {
+public class MainActivity extends AppCompatActivity implements BillingProcessor.IBillingHandler/*DataClient.OnDataChangedListener*/ {
 
     private SharedPreferences sharedPreferences;
     private Switch use24HourTimeSwitch;
@@ -37,6 +39,8 @@ public class MainActivity extends AppCompatActivity /*implements DataClient.OnDa
     private Switch useCelsiusSwitch;
     private Switch showWeatherSwitch;
     private Switch useDarkSkySwitch;
+    private Switch useEuropeanDateFormatSwitch;
+    private Switch showTemperatureDecimalSwitch;
 
     private EditText darkSkyKeyEditText;
 
@@ -44,8 +48,11 @@ public class MainActivity extends AppCompatActivity /*implements DataClient.OnDa
     private boolean showTemperature;
     private boolean useCelsius;
     private boolean showWeather;
+    private boolean useEuropeanDateFormat;
+    private boolean showTemperatureDecimalPoint;
     private String darkSkyAPIKey;
     private boolean useDarkSky;
+    private BillingProcessor bp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,6 +66,10 @@ public class MainActivity extends AppCompatActivity /*implements DataClient.OnDa
         useCelsiusSwitch = findViewById(R.id.celsiusSwitch);
         showWeatherSwitch = findViewById(R.id.weatherSwitch);
         useDarkSkySwitch = findViewById(R.id.useDarkSkySwitch);
+        useEuropeanDateFormatSwitch = findViewById(R.id.dateFormatSwitch);
+        showTemperatureDecimalSwitch = findViewById(R.id.temperaturePrecisionSwitch);
+
+
 
         darkSkyKeyEditText = findViewById(R.id.darkSkyEditText);
 
@@ -97,6 +108,22 @@ public class MainActivity extends AppCompatActivity /*implements DataClient.OnDa
             }
         });
 
+        useEuropeanDateFormatSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                sharedPreferences.edit().putBoolean("use_european_date", isChecked).apply();
+                syncToWear();
+            }
+        });
+
+        showTemperatureDecimalSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                sharedPreferences.edit().putBoolean("show_temperature_decimal", isChecked).apply();
+                syncToWear();
+            }
+        });
+
         useDarkSkySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -122,6 +149,10 @@ public class MainActivity extends AppCompatActivity /*implements DataClient.OnDa
 
             }
         });
+
+        //TODO put actual Google Play license key here
+        bp = new BillingProcessor(this, "PLACEHOLDER", this);
+        bp.initialize();
     }
 
     private void loadPreferences(){
@@ -129,6 +160,10 @@ public class MainActivity extends AppCompatActivity /*implements DataClient.OnDa
         showTemperature = sharedPreferences.getBoolean("show_temperature", false);
         useCelsius = sharedPreferences.getBoolean("use_celsius", false);
         showWeather = sharedPreferences.getBoolean("show_weather", false);
+        useEuropeanDateFormat = sharedPreferences.getBoolean("use_european_date", false);
+        showTemperatureDecimalPoint = sharedPreferences.getBoolean("show_temperature_decimal", false);
+
+
         darkSkyAPIKey = sharedPreferences.getString("dark_sky_api_key", "");
         useDarkSky = sharedPreferences.getBoolean("use_dark_sky", false);
     }
@@ -153,6 +188,8 @@ public class MainActivity extends AppCompatActivity /*implements DataClient.OnDa
         dataMap.putBoolean("show_temperature", showTemperature);
         dataMap.putBoolean("use_celsius", useCelsius);
         dataMap.putBoolean("show_weather", showWeather);
+        dataMap.putBoolean("use_european_date", useEuropeanDateFormat);
+        dataMap.putBoolean("show_temperature_decimal", showTemperatureDecimalPoint);
         dataMap.putString("dark_sky_api_key", darkSkyAPIKey);
         dataMap.putBoolean("use_dark_sky", useDarkSky);
 
@@ -182,6 +219,9 @@ public class MainActivity extends AppCompatActivity /*implements DataClient.OnDa
         showTemperatureSwitch.setChecked(showTemperature);
         useCelsiusSwitch.setChecked(useCelsius);
         showWeatherSwitch.setChecked(showWeather);
+        useEuropeanDateFormatSwitch.setChecked(useEuropeanDateFormat);
+        showTemperatureDecimalSwitch.setChecked(showTemperatureDecimalPoint);
+
         useDarkSkySwitch.setChecked(useDarkSky);
         darkSkyKeyEditText.setText(darkSkyAPIKey);
     }
@@ -233,6 +273,37 @@ public class MainActivity extends AppCompatActivity /*implements DataClient.OnDa
     @Override
     public void onDestroy() {
         //Wearable.getDataClient(getApplicationContext()).removeListener(this);
+        if (bp != null) {
+            bp.release();
+        }
+
         super.onDestroy();
+    }
+
+    @Override
+    public void onProductPurchased(String productId, TransactionDetails details) {
+
+    }
+
+    @Override
+    public void onPurchaseHistoryRestored() {
+
+    }
+
+    @Override
+    public void onBillingError(int errorCode, Throwable error) {
+
+    }
+
+    @Override
+    public void onBillingInitialized() {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (!bp.handleActivityResult(requestCode, resultCode, data)) {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }

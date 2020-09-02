e878092b79d209b663694583f24bf28a8c9bee3a
Cole Gerdemann <corvettecole@gmail.com>
2020-09-02 12:40:47 -0400

    remove old files, prepare for full phone app rewrite

---
+++ phoneApp/src/main/java/com/corvettecole/pixelwatchface/ui/NavigationActivity.java
@@ -0,0 +1,14 @@
+package com.corvettecole.pixelwatchface.ui;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.corvettecole.pixelwatchface.R;
+
+public class NavigationActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_navigation);
+  }
+}
